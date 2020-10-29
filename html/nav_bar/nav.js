550ce2fcece6358a81b9c4512a574a3f42aecbb3
Michael Easter <codetojoy@gmail.com>
2020-10-29 10:16:05 -0300

    experiment

--- html/nav_bar/nav.js
+++ html/nav_bar/nav.js
@@ -1,5 +1,5 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "70%" 
+    document.getElementById("mySidenav").style.width = "50%" 
     document.getElementById('backdrop').style.display = "block" 
 }
 
