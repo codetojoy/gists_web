17e16349cb4687159a288c177c90594a622f59e8
Michael Easter <codetojoy@gmail.com>
2020-11-03 10:05:02 -0400

    incremental update Tue  3 Nov 2020 10:05:02 AST

---
+++ svelte/repl/svelte-and-sapper-in-action/ch10/pie/app.svelte
@@ -0,0 +1,15 @@
+<script>
+  import {tweened} from 'svelte/motion';
+  import Pie from './Pie.svelte';
+ 
+  let percent = 0;
+  const store = tweened(0, {duration: 1000});
+  // const store = spring(0, {stiffness: 0.3, damping: 0.3});
+  $: store.set(percent || 0);                  
+</script>
+ 
+<label>
+  Percent
+  <input type="number" min="0" max="100" bind:value={percent}>
+</label>
+<Pie size={200} percent={$store} />
