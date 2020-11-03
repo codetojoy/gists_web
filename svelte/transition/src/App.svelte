a6720605c4f6e241138173fc1a269444b4ddee25
Michael Easter <codetojoy@gmail.com>
2020-11-03 10:16:56 -0400

    incremental update Tue  3 Nov 2020 10:16:56 AST

---
+++ svelte/transition/src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+	import entries from "./stores/entries";
+	import ControlPanel from "./ControlPanel.svelte";
+	import Items from "./Items.svelte";
+
+	export let name;
+
+	const now = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', second:'2-digit'});
+</script>
+
+<div class="outer">
+	<section class="controls">
+		<ControlPanel 
+			on:addRandom={() => entries.addRandom()}
+			on:reset={() => entries.reset()}
+			on:clearAll={() => entries.clearAll()}
+		/>
+	</section>
+	<section>
+		<div class="primary">
+			<h3>TRACER {name} ({now})</h3>
+			<Items items={$entries}>
+			</Items>
+		</div>
+	</section>
+</div>
+
+<style>
+	.controls {
+		flex-grow: 0;
+		flex-shrink: 0;
+		flex-direction: column;
+		width: 20%;
+	}	
+
+	.outer {
+		display: flex;
+		flex-direction: row;
+    }
+
+	.primary {
+	}
+
+	h3 {
+		color: #ff3e00;
+	}
+</style>
---
+++ svelte/transition/src/Items.svelte
@@ -0,0 +1,20 @@
+
+<script>
+    import { linear } from 'svelte/easing';
+    import {blur, fade, fly, scale, slide} from 'svelte/transition';
+
+    import Item from './Item.svelte';
+
+    export let items = [];
+
+    const options = {duration: 1000, easing: linear};
+</script>
+
+{#each items as item (item)}
+    <div 
+        in:fly={{...options, opacity: 1, x: -400}}
+        out:fly={{...options, opacity: 1, x: 500}}
+    >
+        <Item name={item} />
+    </div>
+{/each}
