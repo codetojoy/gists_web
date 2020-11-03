a5464ca29b8562f0995e35ab3d6413ae3b003536
Michael Easter <codetojoy@gmail.com>
2020-11-03 17:21:07 -0400

    incremental update Tue  3 Nov 2020 17:21:07 AST

---
+++ svelte/repl/svelte-and-sapper-in-action/ch11/reactive_statement.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let radius = 1;
+  $: area = 2 * Math.PI * radius**2;
+  $: console.log(`TRACER change detected! radius: ${radius}`);
+</script>
+
+<main>
+  <label>
+    Circle Radius:
+    <input type="number" bind:value={radius}>
+  </label>
+  <label>
+    Circle Area: {area}
+  </label>
+</main>
