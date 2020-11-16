a50612dad1851fe1b965cc14f2930cdd63211c8c
Michael Easter <codetojoy@gmail.com>
2020-11-16 19:48:45 -0400

    new test via Jest

---
+++ svelte/simple_sandbox_unit_test/src/App.spec.js
@@ -0,0 +1,64 @@
+import { cleanup, render, waitFor, fireEvent } from "@testing-library/svelte";
+
+import App from "./App.svelte";
+
+describe("App", () => {
+  const NUM_SEED_ITEMS = 20;
+  const NUM_NO_ITEMS = 0;
+
+  afterEach(cleanup);
+
+  function expectItemCount(count) {
+    return waitFor(() => {
+      const paragraphs = document.querySelectorAll("p");
+      expect(paragraphs).not.toBeNull();
+      expect(paragraphs.length).toBe(count);
+    });
+  }
+
+  function expectText(getByText, text) {
+    return waitFor(() => {
+      expect(getByText(text)).not.toBeNull();
+    });
+  }
+
+  test("should render basic", async () => {
+    // test
+    const { getByText } = render(App, { name: "MyApp" });
+
+    expectItemCount(NUM_SEED_ITEMS);
+  });
+
+  test("should add an item", async () => {
+    const { getByText, getByTestId } = render(App, { name: "MyApp" });
+
+    // test
+    const button = getByTestId("add-random");
+    fireEvent.click(button);
+
+    expectItemCount(NUM_SEED_ITEMS + 1);
+    expectText("AutoGen1");
+  });
+
+  test("should clear new items on reset", async () => {
+    const { getByTestId } = render(App, { name: "MyApp" });
+
+    const randomButton = getByTestId("add-random");
+    fireEvent.click(randomButton);
+
+    const resetButton = getByTestId("reset");
+    fireEvent.click(resetButton);
+
+    expectItemCount(NUM_SEED_ITEMS);
+  });
+
+  test("should clear all items", async () => {
+    const { getByTestId } = render(App, { name: "MyApp" });
+
+    // test
+    const clearAllButton = getByTestId("clear-all");
+    fireEvent.click(clearAllButton);
+
+    expectItemCount(NUM_NO_ITEMS);
+  });
+});
