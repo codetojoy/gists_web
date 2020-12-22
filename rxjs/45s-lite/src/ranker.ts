37aba912a6a274571d6b87eff8163de60f466392
Michael Easter <codetojoy@gmail.com>
2020-12-22 10:19:36 -0400

    incremental update Tue 22 Dec 2020 10:19:36 AST

--- rxjs/45s-lite/src/ranker.ts
+++ rxjs/45s-lite/src/ranker.ts
@@ -28,130 +28,182 @@ export class Ranker {
     let value: number = 0;
 
     // off-suit CLUBS
-    this.init(this._offSuitBlack, Ordinal.KING, Suit.CLUBS, 12);
-    this.init(this._offSuitBlack, Ordinal.QUEEN, Suit.CLUBS, 11);
-    this.init(this._offSuitBlack, Ordinal.JACK, Suit.CLUBS, 10);
-    this.init(this._offSuitBlack, Ordinal.ACE, Suit.CLUBS, 9);
-    this.init(this._offSuitBlack, Ordinal.TWO, Suit.CLUBS, 8);
-    this.init(this._offSuitBlack, Ordinal.THREE, Suit.CLUBS, 7);
-    this.init(this._offSuitBlack, Ordinal.FOUR, Suit.CLUBS, 6);
-    this.init(this._offSuitBlack, Ordinal.FIVE, Suit.CLUBS, 5);
-    this.init(this._offSuitBlack, Ordinal.SIX, Suit.CLUBS, 4);
-    this.init(this._offSuitBlack, Ordinal.SEVEN, Suit.CLUBS, 3);
-    this.init(this._offSuitBlack, Ordinal.EIGHT, Suit.CLUBS, 2);
-    this.init(this._offSuitBlack, Ordinal.NINE, Suit.CLUBS, 1);
-    this.init(this._offSuitBlack, Ordinal.TEN, Suit.CLUBS, 0);
+    value = 1;
+    this.init(this._offSuitBlack, Ordinal.TEN, Suit.CLUBS, value++);
+    this.init(this._offSuitBlack, Ordinal.NINE, Suit.CLUBS, value++);
+    this.init(this._offSuitBlack, Ordinal.EIGHT, Suit.CLUBS, value++);
+    this.init(this._offSuitBlack, Ordinal.SEVEN, Suit.CLUBS, value++);
+    this.init(this._offSuitBlack, Ordinal.SIX, Suit.CLUBS, value++);
+    this.init(this._offSuitBlack, Ordinal.FIVE, Suit.CLUBS, value++);
+    this.init(this._offSuitBlack, Ordinal.FOUR, Suit.CLUBS, value++);
+    this.init(this._offSuitBlack, Ordinal.THREE, Suit.CLUBS, value++);
+    this.init(this._offSuitBlack, Ordinal.TWO, Suit.CLUBS, value++);
+    this.init(this._offSuitBlack, Ordinal.ACE, Suit.CLUBS, value++);
+    this.init(this._offSuitBlack, Ordinal.JACK, Suit.CLUBS, value++);
+    this.init(this._offSuitBlack, Ordinal.QUEEN, Suit.CLUBS, value++);
+    this.init(this._offSuitBlack, Ordinal.KING, Suit.CLUBS, value++);
+
+    if (value != 14) {
+      throw new TypeError(`internal error value: ${value}`);
+    }
 
     // off-suit SPADES
-    this.init(this._offSuitBlack, Ordinal.KING, Suit.SPADES, 12);
-    this.init(this._offSuitBlack, Ordinal.QUEEN, Suit.SPADES, 11);
-    this.init(this._offSuitBlack, Ordinal.JACK, Suit.SPADES, 10);
-    this.init(this._offSuitBlack, Ordinal.ACE, Suit.SPADES, 9);
-    this.init(this._offSuitBlack, Ordinal.TWO, Suit.SPADES, 8);
-    this.init(this._offSuitBlack, Ordinal.THREE, Suit.SPADES, 7);
-    this.init(this._offSuitBlack, Ordinal.FOUR, Suit.SPADES, 6);
-    this.init(this._offSuitBlack, Ordinal.FIVE, Suit.SPADES, 5);
-    this.init(this._offSuitBlack, Ordinal.SIX, Suit.SPADES, 4);
-    this.init(this._offSuitBlack, Ordinal.SEVEN, Suit.SPADES, 3);
-    this.init(this._offSuitBlack, Ordinal.EIGHT, Suit.SPADES, 2);
-    this.init(this._offSuitBlack, Ordinal.NINE, Suit.SPADES, 1);
-    this.init(this._offSuitBlack, Ordinal.TEN, Suit.SPADES, 0);
+    value = 1;
+    this.init(this._offSuitBlack, Ordinal.TEN, Suit.SPADES, value++);
+    this.init(this._offSuitBlack, Ordinal.NINE, Suit.SPADES, value++);
+    this.init(this._offSuitBlack, Ordinal.EIGHT, Suit.SPADES, value++);
+    this.init(this._offSuitBlack, Ordinal.SEVEN, Suit.SPADES, value++);
+    this.init(this._offSuitBlack, Ordinal.SIX, Suit.SPADES, value++);
+    this.init(this._offSuitBlack, Ordinal.FIVE, Suit.SPADES, value++);
+    this.init(this._offSuitBlack, Ordinal.FOUR, Suit.SPADES, value++);
+    this.init(this._offSuitBlack, Ordinal.THREE, Suit.SPADES, value++);
+    this.init(this._offSuitBlack, Ordinal.TWO, Suit.SPADES, value++);
+    this.init(this._offSuitBlack, Ordinal.ACE, Suit.SPADES, value++);
+    this.init(this._offSuitBlack, Ordinal.JACK, Suit.SPADES, value++);
+    this.init(this._offSuitBlack, Ordinal.QUEEN, Suit.SPADES, value++);
+    this.init(this._offSuitBlack, Ordinal.KING, Suit.SPADES, value++);
+
+    if (value != 14) {
+      throw new TypeError(`internal error value: ${value}`);
+    }
 
     // off-suit DIAMONDS
-    this.init(this._offSuitRed, Ordinal.KING, Suit.DIAMONDS, 12);
-    this.init(this._offSuitRed, Ordinal.QUEEN, Suit.DIAMONDS, 11);
-    this.init(this._offSuitRed, Ordinal.JACK, Suit.DIAMONDS, 10);
-    this.init(this._offSuitRed, Ordinal.TEN, Suit.DIAMONDS, 9);
-    this.init(this._offSuitRed, Ordinal.NINE, Suit.DIAMONDS, 8);
-    this.init(this._offSuitRed, Ordinal.EIGHT, Suit.DIAMONDS, 7);
-    this.init(this._offSuitRed, Ordinal.SEVEN, Suit.DIAMONDS, 6);
-    this.init(this._offSuitRed, Ordinal.SIX, Suit.DIAMONDS, 5);
-    this.init(this._offSuitRed, Ordinal.FIVE, Suit.DIAMONDS, 4);
-    this.init(this._offSuitRed, Ordinal.FOUR, Suit.DIAMONDS, 3);
-    this.init(this._offSuitRed, Ordinal.THREE, Suit.DIAMONDS, 2);
-    this.init(this._offSuitRed, Ordinal.TWO, Suit.DIAMONDS, 1);
-    this.init(this._offSuitRed, Ordinal.ACE, Suit.DIAMONDS, 0);
+    value = 1;
+    this.init(this._offSuitRed, Ordinal.ACE, Suit.DIAMONDS, value++);
+    this.init(this._offSuitRed, Ordinal.TWO, Suit.DIAMONDS, value++);
+    this.init(this._offSuitRed, Ordinal.THREE, Suit.DIAMONDS, value++);
+    this.init(this._offSuitRed, Ordinal.FOUR, Suit.DIAMONDS, value++);
+    this.init(this._offSuitRed, Ordinal.FIVE, Suit.DIAMONDS, value++);
+    this.init(this._offSuitRed, Ordinal.SIX, Suit.DIAMONDS, value++);
+    this.init(this._offSuitRed, Ordinal.SEVEN, Suit.DIAMONDS, value++);
+    this.init(this._offSuitRed, Ordinal.EIGHT, Suit.DIAMONDS, value++);
+    this.init(this._offSuitRed, Ordinal.NINE, Suit.DIAMONDS, value++);
+    this.init(this._offSuitRed, Ordinal.TEN, Suit.DIAMONDS, value++);
+    this.init(this._offSuitRed, Ordinal.JACK, Suit.DIAMONDS, value++);
+    this.init(this._offSuitRed, Ordinal.QUEEN, Suit.DIAMONDS, value++);
+    this.init(this._offSuitRed, Ordinal.KING, Suit.DIAMONDS, value++);
+
+    if (value != 14) {
+      throw new TypeError(`internal error value: ${value}`);
+    }
 
     // off-suit HEARTS
-    this.init(this._offSuitRed, Ordinal.KING, Suit.HEARTS, 12);
-    this.init(this._offSuitRed, Ordinal.QUEEN, Suit.HEARTS, 11);
-    this.init(this._offSuitRed, Ordinal.JACK, Suit.HEARTS, 10);
-    this.init(this._offSuitRed, Ordinal.TEN, Suit.HEARTS, 9);
-    this.init(this._offSuitRed, Ordinal.NINE, Suit.HEARTS, 8);
-    this.init(this._offSuitRed, Ordinal.EIGHT, Suit.HEARTS, 7);
-    this.init(this._offSuitRed, Ordinal.SEVEN, Suit.HEARTS, 6);
-    this.init(this._offSuitRed, Ordinal.SIX, Suit.HEARTS, 5);
-    this.init(this._offSuitRed, Ordinal.FIVE, Suit.HEARTS, 4);
-    this.init(this._offSuitRed, Ordinal.FOUR, Suit.HEARTS, 3);
-    this.init(this._offSuitRed, Ordinal.THREE, Suit.HEARTS, 2);
-    this.init(this._offSuitRed, Ordinal.TWO, Suit.HEARTS, 1);
+    value = 2;
+    this.init(this._offSuitRed, Ordinal.TWO, Suit.HEARTS, value++);
+    this.init(this._offSuitRed, Ordinal.THREE, Suit.HEARTS, value++);
+    this.init(this._offSuitRed, Ordinal.FOUR, Suit.HEARTS, value++);
+    this.init(this._offSuitRed, Ordinal.FIVE, Suit.HEARTS, value++);
+    this.init(this._offSuitRed, Ordinal.SIX, Suit.HEARTS, value++);
+    this.init(this._offSuitRed, Ordinal.SEVEN, Suit.HEARTS, value++);
+    this.init(this._offSuitRed, Ordinal.EIGHT, Suit.HEARTS, value++);
+    this.init(this._offSuitRed, Ordinal.NINE, Suit.HEARTS, value++);
+    this.init(this._offSuitRed, Ordinal.TEN, Suit.HEARTS, value++);
+    // this.init(this._offSuitRed, Ordinal.ACE, Suit.HEARTS, value++);
+    this.init(this._offSuitRed, Ordinal.JACK, Suit.HEARTS, value++);
+    this.init(this._offSuitRed, Ordinal.QUEEN, Suit.HEARTS, value++);
+    this.init(this._offSuitRed, Ordinal.KING, Suit.HEARTS, value++);
+
+    if (value != 14) {
+      throw new TypeError(`internal error value: ${value}`);
+    }
 
     // trump CLUBS
+    value = 1;
     let suit: Suit = Suit.CLUBS;
-    this.init(this._trumpClubs, Ordinal.FIVE, suit, 14);
-    this.init(this._trumpClubs, Ordinal.JACK, suit, 13);
-    this.init(this._trumpClubs, Ordinal.ACE, Suit.HEARTS, 12);
-    this.init(this._trumpClubs, Ordinal.ACE, suit, 11);
-    this.init(this._trumpClubs, Ordinal.KING, suit, 10);
-    this.init(this._trumpClubs, Ordinal.QUEEN, suit, 9);
-    this.init(this._trumpClubs, Ordinal.TWO, suit, 8);
-    this.init(this._trumpClubs, Ordinal.THREE, suit, 7);
-    this.init(this._trumpClubs, Ordinal.FOUR, suit, 6);
-    this.init(this._trumpClubs, Ordinal.SIX, suit, 5);
-    this.init(this._trumpClubs, Ordinal.SEVEN, suit, 4);
-    this.init(this._trumpClubs, Ordinal.EIGHT, suit, 3);
-    this.init(this._trumpClubs, Ordinal.NINE, suit, 2);
-    this.init(this._trumpClubs, Ordinal.TEN, suit, 1);
+    this.init(this._trumpClubs, Ordinal.TEN, suit, value++);
+    this.init(this._trumpClubs, Ordinal.NINE, suit, value++);
+    this.init(this._trumpClubs, Ordinal.EIGHT, suit, value++);
+    this.init(this._trumpClubs, Ordinal.SEVEN, suit, value++);
+    this.init(this._trumpClubs, Ordinal.SIX, suit, value++);
+    this.init(this._trumpClubs, Ordinal.FOUR, suit, value++);
+    value++;
+    this.init(this._trumpClubs, Ordinal.THREE, suit, value++);
+    this.init(this._trumpClubs, Ordinal.TWO, suit, value++);
+    value++;
+    value++;
+    this.init(this._trumpClubs, Ordinal.QUEEN, suit, value++);
+    this.init(this._trumpClubs, Ordinal.KING, suit, value++);
+    this.init(this._trumpClubs, Ordinal.ACE, suit, value++);
+    this.init(this._trumpClubs, Ordinal.ACE, Suit.HEARTS, value++);
+    this.init(this._trumpClubs, Ordinal.JACK, suit, value++);
+    this.init(this._trumpClubs, Ordinal.FIVE, suit, value++);
+
+    if (value != 18) {
+      throw new TypeError(`internal error value: ${value}`);
+    }
 
     // trump SPADES
     suit = Suit.SPADES;
-    this.init(this._trumpSpades, Ordinal.FIVE, suit, 14);
-    this.init(this._trumpSpades, Ordinal.JACK, suit, 13);
-    this.init(this._trumpSpades, Ordinal.ACE, Suit.HEARTS, 12);
-    this.init(this._trumpSpades, Ordinal.ACE, suit, 11);
-    this.init(this._trumpSpades, Ordinal.KING, suit, 10);
-    this.init(this._trumpSpades, Ordinal.QUEEN, suit, 9);
-    this.init(this._trumpSpades, Ordinal.TWO, suit, 8);
-    this.init(this._trumpSpades, Ordinal.THREE, suit, 7);
-    this.init(this._trumpSpades, Ordinal.FOUR, suit, 6);
-    this.init(this._trumpSpades, Ordinal.SIX, suit, 5);
-    this.init(this._trumpSpades, Ordinal.SEVEN, suit, 4);
-    this.init(this._trumpSpades, Ordinal.EIGHT, suit, 3);
-    this.init(this._trumpSpades, Ordinal.NINE, suit, 2);
-    this.init(this._trumpSpades, Ordinal.TEN, suit, 1);
+    value = 1;
+    this.init(this._trumpSpades, Ordinal.TEN, suit, value++);
+    this.init(this._trumpSpades, Ordinal.NINE, suit, value++);
+    this.init(this._trumpSpades, Ordinal.EIGHT, suit, value++);
+    this.init(this._trumpSpades, Ordinal.SEVEN, suit, value++);
+    this.init(this._trumpSpades, Ordinal.SIX, suit, value++);
+    value++;
+    this.init(this._trumpSpades, Ordinal.FOUR, suit, value++);
+    this.init(this._trumpSpades, Ordinal.THREE, suit, value++);
+    this.init(this._trumpSpades, Ordinal.TWO, suit, value++);
+    value++;
+    value++;
+    this.init(this._trumpSpades, Ordinal.QUEEN, suit, value++);
+    this.init(this._trumpSpades, Ordinal.KING, suit, value++);
+    this.init(this._trumpSpades, Ordinal.ACE, suit, value++);
+    this.init(this._trumpSpades, Ordinal.ACE, Suit.HEARTS, value++);
+    this.init(this._trumpSpades, Ordinal.JACK, suit, value++);
+    this.init(this._trumpSpades, Ordinal.FIVE, suit, value++);
+
+    if (value != 18) {
+      throw new TypeError(`internal error value: ${value}`);
+    }
 
     // trump DIAMONDS
     suit = Suit.DIAMONDS;
-    this.init(this._trumpDiamonds, Ordinal.FIVE, suit, 14);
-    this.init(this._trumpDiamonds, Ordinal.JACK, suit, 13);
-    this.init(this._trumpDiamonds, Ordinal.ACE, Suit.HEARTS, 12);
-    this.init(this._trumpDiamonds, Ordinal.ACE, suit, 11);
-    this.init(this._trumpDiamonds, Ordinal.KING, suit, 10);
-    this.init(this._trumpDiamonds, Ordinal.QUEEN, suit, 9);
-    this.init(this._trumpDiamonds, Ordinal.TEN, suit, 8);
-    this.init(this._trumpDiamonds, Ordinal.NINE, suit, 7);
-    this.init(this._trumpDiamonds, Ordinal.EIGHT, suit, 6);
-    this.init(this._trumpDiamonds, Ordinal.SEVEN, suit, 5);
-    this.init(this._trumpDiamonds, Ordinal.SIX, suit, 4);
-    this.init(this._trumpDiamonds, Ordinal.FOUR, suit, 3);
-    this.init(this._trumpDiamonds, Ordinal.THREE, suit, 2);
-    this.init(this._trumpDiamonds, Ordinal.TWO, suit, 1);
+    value = 2;
+    this.init(this._trumpDiamonds, Ordinal.TWO, suit, value++);
+    this.init(this._trumpDiamonds, Ordinal.THREE, suit, value++);
+    this.init(this._trumpDiamonds, Ordinal.FOUR, suit, value++);
+    value++;
+    this.init(this._trumpDiamonds, Ordinal.SIX, suit, value++);
+    this.init(this._trumpDiamonds, Ordinal.SEVEN, suit, value++);
+    this.init(this._trumpDiamonds, Ordinal.EIGHT, suit, value++);
+    this.init(this._trumpDiamonds, Ordinal.NINE, suit, value++);
+    this.init(this._trumpDiamonds, Ordinal.TEN, suit, value++);
+    value++;
+    this.init(this._trumpDiamonds, Ordinal.QUEEN, suit, value++);
+    this.init(this._trumpDiamonds, Ordinal.KING, suit, value++);
+    this.init(this._trumpDiamonds, Ordinal.ACE, suit, value++);
+    this.init(this._trumpDiamonds, Ordinal.ACE, Suit.HEARTS, value++);
+    this.init(this._trumpDiamonds, Ordinal.JACK, suit, value++);
+    this.init(this._trumpDiamonds, Ordinal.FIVE, suit, value++);
+
+    if (value != 18) {
+      throw new TypeError(`internal error value: ${value}`);
+    }
 
     // trump HEARTS
     suit = Suit.HEARTS;
-    this.init(this._trumpHearts, Ordinal.FIVE, suit, 14);
-    this.init(this._trumpHearts, Ordinal.JACK, suit, 13);
-    this.init(this._trumpHearts, Ordinal.ACE, suit, 11);
-    this.init(this._trumpHearts, Ordinal.KING, suit, 10);
-    this.init(this._trumpHearts, Ordinal.QUEEN, suit, 9);
-    this.init(this._trumpHearts, Ordinal.TEN, suit, 8);
-    this.init(this._trumpHearts, Ordinal.NINE, suit, 7);
-    this.init(this._trumpHearts, Ordinal.EIGHT, suit, 6);
-    this.init(this._trumpHearts, Ordinal.SEVEN, suit, 5);
-    this.init(this._trumpHearts, Ordinal.SIX, suit, 4);
-    this.init(this._trumpHearts, Ordinal.FOUR, suit, 3);
-    this.init(this._trumpHearts, Ordinal.THREE, suit, 2);
-    this.init(this._trumpHearts, Ordinal.TWO, suit, 1);
+    value = 2;
+    this.init(this._trumpHearts, Ordinal.TWO, suit, value++);
+    this.init(this._trumpHearts, Ordinal.THREE, suit, value++);
+    this.init(this._trumpHearts, Ordinal.FOUR, suit, value++);
+    value++;
+    this.init(this._trumpHearts, Ordinal.SIX, suit, value++);
+    this.init(this._trumpHearts, Ordinal.SEVEN, suit, value++);
+    this.init(this._trumpHearts, Ordinal.EIGHT, suit, value++);
+    this.init(this._trumpHearts, Ordinal.NINE, suit, value++);
+    this.init(this._trumpHearts, Ordinal.TEN, suit, value++);
+    value++;
+    this.init(this._trumpHearts, Ordinal.QUEEN, suit, value++);
+    this.init(this._trumpHearts, Ordinal.KING, suit, value++);
+    value++;
+    this.init(this._trumpHearts, Ordinal.ACE, suit, value++);
+    this.init(this._trumpHearts, Ordinal.JACK, suit, value++);
+    this.init(this._trumpHearts, Ordinal.FIVE, suit, value++);
+
+    if (value != 18) {
+      throw new TypeError(`internal error value: ${value}`);
+    }
   }
 
   compare(a: number, b: number): number {
