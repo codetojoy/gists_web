313510120daa767fced1ab3348b317cd233144f2
Michael Easter <codetojoy@gmail.com>
2020-12-27 11:00:52 -0400

    incremental update Sun 27 Dec 2020 11:00:52 AST

---
+++ rxjs/45s-lite/src/strategy.ts
@@ -0,0 +1,24 @@
+import { Card } from "./card";
+import { Deck } from "./deck";
+import { Config } from "./config";
+import { Table } from "./table";
+import { Hand } from "./hand";
+import { Bid } from "./bid";
+import { Suit } from "./suit";
+import { Ranker } from "./ranker";
+
+export interface Strategy {
+  select(hand: Hand, trumpSuit: Suit, leadingSuit: Suit): Card;
+}
+
+export class Strategies {
+  getStrategy(type: string) {
+    return new DefaultStrategy();
+  }
+}
+
+class DefaultStrategy implements Strategy {
+  public select(hand: Hand, trumpSuit: Suit, leadingSuit: Suit): Card {
+    return hand.cards.shift();
+  }
+}
