83f94a6fdf24c8ad4582631d7aa5e404c1ca6dd9
Michael Easter <codetojoy@gmail.com>
2021-01-20 09:13:05 -0400

    incremental update Wed 20 Jan 2021 09:13:05 AST

--- angular/sandbox/src/app/app.component.ts
+++ angular/sandbox/src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'sandbox';
+  state: string = 'foo';
+
+  isState(s: string): boolean {
+    return this.state === s;
+  }
+
+  isStateFoo(): boolean {
+    return this.isState('foo');
+  }
+  isStateBar(): boolean {
+    return this.isState('bar');
+  }
+  isStateAdmin(): boolean {
+    return this.isState('admin');
+  }
+
+  onSelect(event: string) {
+    console.log(`TRACER app onSelect: ${event}`);
+    this.state = event;
+  }
 }
--- angular/sandbox/src/app/app.module.ts
+++ angular/sandbox/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooComponent } from './foo/foo.component';
+import { BarComponent } from './bar/bar.component';
+import { AdminComponent } from './admin/admin.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooComponent,
+    BarComponent,
+    AdminComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
