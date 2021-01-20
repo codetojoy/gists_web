cec0541dca746b6e0c72ca9b8724bf95fc80e0d5
Michael Easter <codetojoy@gmail.com>
2021-01-20 09:35:39 -0400

    incremental update Wed 20 Jan 2021 09:35:39 AST

--- angular/sandbox/src/app/foo/foo.component.ts
+++ angular/sandbox/src/app/foo/foo.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { NodeWithI18n } from '@angular/compiler';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Util } from '../util/Util';
 
 @Component({
   selector: 'app-foo',
   templateUrl: './foo.component.html',
-  styleUrls: ['./foo.component.css']
+  styleUrls: ['./foo.component.css'],
 })
 export class FooComponent implements OnInit {
+  @ViewChild('logArea', { static: false }) logAreaRef: ElementRef;
 
-  constructor() { }
+  util: Util = new Util();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  writeLog(): void {
+    console.log(this.util.tracer('FC.writeLog'));
+    // we shouldn't be writing to a native element like this
+    this.logAreaRef.nativeElement.innerHTML = this.util.tracer('view child');
   }
-
 }
