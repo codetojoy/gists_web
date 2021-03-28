1ddbf0d67d5ea16df490669bbdfe793c6a9a2cc1
Michael Easter <codetojoy@gmail.com>
2021-03-28 12:49:00 -0300

    more verbs

--- angular/sandbox-http/client.sh
+++ angular/sandbox-http/client.sh
@@ -21,26 +21,58 @@ OPTION=$1
 ### ensure id passed on CLI
 
 if [ -z ${2+x} ]; then 
-echo "please specify id, even if bogus for get-all"
+echo "please specify id, even if bogus placeholder"
 exit -1
 fi
 
 MY_ID=$2
 
-if [ "$OPTION" == "1" ]; then
+if [ "$OPTION" == "0" ]; then
+echo ""
+echo "0 : help"
+echo "1 : get all"
+echo "2 : get by id"
+echo "3 : post new, then get all"
+echo "4 : update by id, then get all"
+echo "5 : delete by id, then get all"
+echo "6 : patch by id, then get all"
+echo ""
+elif [ "$OPTION" == "1" ]; then
 # ----------------- get all
 curl ${MY_API_ROOT}.json?print=pretty
+
 elif [ "$OPTION" == "2" ]; then
 # ----------------- get one
 curl ${MY_API_ROOT}/${MY_ID}.json?print=pretty
+
 elif [ "$OPTION" == "3" ]; then
 # ----------------- post one
 curl -X POST -d '{"name" : "EVH", "strategy" : "MaxCard"}' ${MY_API_ROOT}.json
 echo ""
 echo "post OK ... fetching"
 curl ${MY_API_ROOT}.json?print=pretty
+
+elif [ "$OPTION" == "4" ]; then
+# ----------------- update one
+curl -X PUT -d '{ "name": "EVH2", "strategy": "MinCard" }' ${MY_API_ROOT}/${MY_ID}.json
+echo ""
+echo "put OK ... fetching"
+curl ${MY_API_ROOT}.json?print=pretty
+
+elif [ "$OPTION" == "5" ]; then
+# ----------------- delete one
+curl -X DELETE ${MY_API_ROOT}/${MY_ID}.json
+echo ""
+echo "delete OK ... fetching"
+curl ${MY_API_ROOT}.json?print=pretty
+
+elif [ "$OPTION" == "6" ]; then
+# ----------------- update one
+curl -X PATCH -d '{"strategy":"NearestCard"}' ${MY_API_ROOT}/${MY_ID}.json
+echo ""
+echo "patch OK ... fetching"
+curl ${MY_API_ROOT}.json?print=pretty
 fi 
 
-
-# ----------------- update one
-# curl -X PUT -d '{ "name": "Rhoads", "strategy": "MinCard" }' ${MY_API_ROOT}/${ID}.json
+echo ""
+echo "Ready."
