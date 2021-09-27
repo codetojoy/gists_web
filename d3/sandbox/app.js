e6d569f36a222ecfb087d73c8f14b15c85c2d58c
Michael Easter <codetojoy@gmail.com>
2021-09-27 09:59:13 -0300

    incremental update Mon 27 Sep 2021 09:59:13 ADT

--- d3/sandbox/app.js
+++ d3/sandbox/app.js
@@ -1,6 +1,6 @@
 // TODO: data should come from CSV ?
 const meetupData = {
-  attendees: ['Dustin Sparks', 'Evan Porter', 'Matt Duffy', 'Mike Berger', 'Nolan Phillips'],
+  attendees: ["Dustin Sparks", "Evan Porter", "Matt Duffy", "Mike Berger", "Nolan Phillips"],
   addItem(attendee) {
     this.attendees.push(attendee);
   },
@@ -9,47 +9,65 @@ const meetupData = {
   },
   log() {
     for (const [i, value] of this.attendees.entries()) {
-      console.log('%d: %s', i, value);
+      console.log("%d: %s", i, value);
     }
   },
   // https://stackoverflow.com/a/2450976/12704
   shuffle() {
-      const array = this.attendees;
-      var currentIndex = array.length;
+    const array = this.attendees;
+    var currentIndex = array.length;
 
-      // While there remain elements to shuffle...
-      while (0 !== currentIndex) {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      const randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-        // Pick a remaining element...
-        const randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        const temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-      }
-  }
+      // And swap it with the current element.
+      const temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  },
 };
 
 function update() {
-  d3.select('ul')
-    .selectAll('li')
-    .data(meetupData.attendees, d => d)
+  updateContentA();
+  updateContentB();
+  updateContentC();
+}
+
+function updateContentA() {
+  let svg = d3.select("#content-a");
+  svg.append("circle").attr("cx", 200).attr("cy", 200).attr("r", 40).style("fill", "blue");
+}
+
+function updateContentC() {
+  let svg = d3.select("#content-c");
+  svg.append("circle").attr("cx", 200).attr("cy", 200).attr("r", 40).style("fill", "green");
+}
+
+function updateContentB() {
+  d3.select("ul")
+    .selectAll("li")
+    .data(meetupData.attendees, (d) => d)
     .join(
-      function(enter) {
+      function (enter) {
         console.log(`TRACER enter`);
-        return enter.append('li')
-             // .classed('added', true) // debug
-             .text(d => d);
+        return (
+          enter
+            .append("li")
+            // .classed('added', true) // debug
+            .text((d) => d)
+        );
       },
-      function(update) {
+      function (update) {
         console.log(`TRACER update`);
         return update;
         // debug:
         // return update.classed('updated', true);
       },
-      function(exit) {
+      function (exit) {
         console.log(`TRACER exit`);
         return exit.remove();
         // TODO: exit not working properly when CSS
